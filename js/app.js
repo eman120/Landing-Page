ab876c782e9017b87baaadae780f8ebbae46d98f
eman120 <76956637+eman120@users.noreply.github.com>
2021-09-27 23:52:17 +0200

    add app.js file

---
+++ js/app.js
@@ -0,0 +1,75 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+*/
+
+/**
+ * Define Global Variables
+ *
+*/
+const sects = document.querySelectorAll('section');
+const listItemsNo = sects.length;
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+function createItems() {
+    const menu = document.getElementById('navbar__list');
+    for (const sect of sects) {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<a class = 'menu__link' href = '#${sect.getAttribute('id')}'>${sect.getAttribute('data-nav')}</a>`;
+        menu.appendChild(listItem);
+    }
+}
+
+function active() {
+    sects.forEach(section => {
+        section.addEventListener('click', function () {
+            sects.forEach(sect => sect.classList.remove('your-active-class'));
+            this.classList.add('your-active-class');        
+        });
+    });
+}
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+*/
+
+// build the nav
+createItems();
+
+// Add class 'active' to section when near top of viewport
+
+document.addEventListener('scroll', active);
+
+// Scroll to anchor ID using scrollTO event
+let scrollSmooth = document.querySelector('html');
+scrollSmooth.scrollIntoView({behavior:"smooth", block:"end"});
+/**
+ * End Main Functions
+ * Begin Events
+ *
+*/
+
+// Build menu
+
+// Scroll to section on link click
+
+// Set sects as active
+
+
